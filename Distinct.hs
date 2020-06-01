2874113686ebcb210086383a3ca7b322c209d54e
Stanisław Wasik <st.wasik@gmail.com>
2020-06-01 21:47:47 +0200

    Updated Distinct module

--- Distinct.hs
+++ Distinct.hs
@@ -6,12 +6,8 @@ import qualified Data.List as List
 import qualified Data.Set  as Set
 
 distinct :: Ord a => [a] -> [a]
-distinct list 
-    | (length list) < 100 = List.nub list
-    | otherwise = distinct' list [] Set.empty
-
-distinct' :: Ord a => [a] -> [a] -> Set.Set a -> [a]
-distinct' [] out set = List.reverse out
-distinct' (x:xs) out set
-    | x `Set.member` set = distinct' xs out set
-    | otherwise = distinct' xs (x:out) (x `Set.insert` set) 
+distinct list = snd $ foldl f (Set.empty, []) list
+    where 
+        f (set, acc) e
+            | e `Set.member` set = (set, acc)
+            | otherwise = (e `Set.insert` set, acc ++ [e])
